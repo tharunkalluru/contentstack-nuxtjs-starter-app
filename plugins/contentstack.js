d614d01bf28bca3616c2cd7b14c673b72b22feaf
Amitkanswal <amitkanswal7@gmail.com>
2022-01-31 17:15:52 +0530

    fix: updated dependencies [ECO-582]

--- plugins/contentstack.js
+++ plugins/contentstack.js
@@ -10,7 +10,8 @@ const Stack = contentstack.Stack({
     : 'us',
 })
 
-if(process.env.CONTENTSTACK_API_HOST) Stack.setHost(process.env.CONTENTSTACK_API_HOST);
+if (process.env.CONTENTSTACK_API_HOST)
+  Stack.setHost(process.env.CONTENTSTACK_API_HOST)
 
 const renderOption = {
   ['span']: (node, next) => {

6fbeb1fcab307674dd97ae0fb3f32aece25e767b
tharunkalluru <tharun.kalluru@contentstack.com>
2025-04-08 16:59:51 -0400

    Added env compatibilty for launch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default {
 
   components: true,
 
-  buildModules: ['@nuxtjs/dotenv','@nuxt/typescript-build', '@nuxtjs/eslint-module'],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/eslint-module'],
 
   modules: [
     '@nuxtjs/axios',
